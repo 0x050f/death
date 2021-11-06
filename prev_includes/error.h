c328ec7e9a45a71e34f4d2edcf7dc76a7e4d8d62
ska42 <skalyxe00@gmail.com>
2021-11-06 07:08:04 -0400

    remove prev srcs

--- prev_includes/error.h
+++
@@ -1,9 +0,0 @@
-#ifndef ERROR_H
-# define ERROR_H
-
-# define CORRUPTED_FILE -1
-# define ALREADY_INFECTED -2
-# define NOT_ELF -3
-# define TOO_BIG -4
-
-#endif
--- prev_srcs/main.c
+++
@@ -1,8 +0,0 @@
-#include "famine.h"
-
-int		_start(void)
-{
-	infect();
-	syscall_exit(0);
-	return (0);
-}
