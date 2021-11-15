00f818d9835dbca3d79d0e4b09d43c5d83742819
ska42 <skalyxe00@gmail.com>
2021-11-15 20:33:10 +0100

    add tests

--- test/script.sh
+++ test/script.sh
@@ -12,42 +12,116 @@ signature="Famine version 1.0 (c)oded by lmartin"
 test_path_no_exec() {
 	rm -rf /tmp/test /tmp/test2
 	output=$(./$exec)
-	assertEquals "$output" ""
+	assertEquals "" "$output"
 	mkdir -p /tmp/test
 	output=$(./$exec)
-	assertEquals "$output" ""
+	assertEquals "" "$output"
 	mkdir -p /tmp/test2
 	output=$(./$exec)
-	assertEquals "$output" ""
+	assertEquals "" "$output"
 	rm -rf /tmp/test
 	output=$(./$exec)
-	assertEquals "$output" ""
+	assertEquals "" "$output"
 	mkdir -p /tmp/test
 	output=$(./$exec)
-	assertEquals "$output" ""
+	assertEquals "" "$output"
 }
 
 test_host_infection() {
 	cp -f /bin/ls /tmp/test/ls
 	output=$(strings /tmp/test/ls | grep Famine)
-	assertEquals "$ouput" ""
+	assertEquals "" "$output"
 	./$exec
 	output=$(strings /tmp/test/ls | grep Famine)
-	assertEquals "$output" "$signature"
+	assertEquals "$signature" "$output"
 	output=$(/tmp/test/ls /tmp/test)
-	assertEquals "$output" "ls"
+	assertEquals "ls" "$output"
 }
 
 test_simple_infection() {
 	cp -f /bin/pwd /tmp/test2/pwd
 	output=$(strings /tmp/test2/pwd | grep Famine)
-	assertEquals "$output" ""
+	assertEquals "" "$output"
 	/tmp/test/ls &> /dev/null
 	output=$(strings /tmp/test2/pwd | grep Famine)
-	assertEquals "$output" "$signature"
+	assertEquals "$signature" "$output"
 	output_cmd=$(/bin/pwd)
 	output=$(/tmp/test2/pwd)
-	assertEquals "$output" "$output_cmd"
+	assertEquals "$output_cmd" "$output"
+}
+
+test_subdir_infection() {
+	mkdir -p /tmp/test/lol/xd
+	cp -f /bin/ls /tmp/test/lol
+	cp -f /bin/pwd /tmp/test/lol/xd
+	output=$(strings /tmp/test/lol/ls | grep Famine)
+	assertEquals "" "$output"
+	output=$(strings /tmp/test/lol/xd/pwd | grep Famine)
+	assertEquals "" "$output"
+
+	# test from host
+	./$exec
+	output=$(strings /tmp/test/lol/ls | grep Famine)
+	assertEquals "$signature" "$output"
+	output=$(strings /tmp/test/lol/xd/pwd | grep Famine)
+	assertEquals "$signature" "$output"
+
+	cp -f /bin/ls /tmp/test
+	./$exec
+	# test from infected file
+	cp -f /bin/ls /tmp/test/lol
+	cp -f /bin/pwd /tmp/test/lol/xd
+	output=$(/tmp/test/ls)
+	output=$(strings /tmp/test/lol/ls | grep Famine)
+	assertEquals "$signature" "$output"
+	output=$(strings /tmp/test/lol/xd/pwd | grep Famine)
+	assertEquals "$signature" "$output"
+}
+
+test_process_no_infection() {
+	cp -f /bin/ls /tmp/test/ls
+	output=$(strings /tmp/test/ls | grep Famine)
+	assertEquals "" "$output"
+
+	# test from host
+	cat /dev/zero &
+	pid=$!
+	./$exec
+	output=$(strings /tmp/test/ls | grep Famine)
+	assertEquals "" "$output"
+	output_cmd=$(/bin/ls)
+	output=$(/tmp/test/ls)
+	assertEquals "$output_cmd" "$output"
+	kill -9 $pid
+
+	# infect /tmp/test/ls
+	./$exec
+	output=$(strings /tmp/test/ls | grep Famine)
+	assertEquals "$signature" "$output"
+	output_cmd=$(/bin/ls)
+	output=$(/tmp/test/ls)
+	assertEquals "$output_cmd" "$output"
+
+	# test from /tmp/test/ls infected
+	cp -f /bin/pwd /tmp/test2/pwd
+	cat /dev/zero &
+	pid=$!
+	output_cmd=$(/bin/ls)
+	output=$(/tmp/test/ls)
+	assertEquals "$output_cmd" "$output"
+	output=$(strings /tmp/test2/pwd | grep Famine)
+	assertEquals "" "$output"
+	kill -9 $pid
+
+	# infect
+	output_cmd=$(/bin/ls)
+	output=$(/tmp/test/ls)
+	assertEquals "$output_cmd" "$output"
+	output=$(strings /tmp/test2/pwd | grep Famine)
+	assertEquals "$signature" "$output"
+	output_cmd=$(/bin/pwd)
+	output=$(/tmp/test2/pwd)
+	assertEquals "$output_cmd" "$output"
 }
 
 test_all_bin_binaries() {
