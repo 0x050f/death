ae41f78cac896b4ef82b07cc29e84b853f44b3bd
ska42 <skalyxe00@gmail.com>
2021-11-18 19:08:12 +0100

    count packing

--- poc_packer.c
+++ poc_packer.c
@@ -1,3 +1,5 @@
+#define _GNU_SOURCE
+
 #include <stdio.h>
 #include <unistd.h>
 #include <fcntl.h>
@@ -6,9 +8,66 @@
 #include <string.h>
 #include <elf.h>
 
+typedef struct	s_data // 3 bytes
+{
+	unsigned char d; // distance
+	unsigned char l; // length
+	char c; // char
+}				t_data;
+
 void	pack(void *addr, size_t size)
 {
-	write(STDOUT_FILENO, addr, size);
+	char	*buffer = addr;
+	int		size_b = 256;
+	char	*dictionary = addr;
+
+	t_data compression[size];
+
+	size_t i = 0;
+	size_t j = 0;
+	while (i < size)
+	{
+		size_t len = dictionary - buffer;
+		if (len > size_b)
+		{
+			buffer += len - size_b;
+			len = dictionary - buffer;
+		}
+//		printf("\n");
+//		printf("len: %ld\n", len);
+		char distance;
+		char length;
+		char c;
+		char *ret = 0;
+		char *prev_ret = 0;
+		size_t k = 0;
+		while (k < size_b && i + k < size && (ret = (char *)memmem((void *)buffer, len, (void *)dictionary, k)))
+		{
+			prev_ret = ret;
+			k++;
+		}
+		if (!prev_ret && i + k == size)
+			printf("hello !\n");
+//		printf("dictionary 0x%lx\n", (unsigned long)dictionary);
+//		printf("prev_ret 0x%lx\n", (unsigned long)prev_ret);
+		k--;
+		compression[j].d = (char)((unsigned long)dictionary - (unsigned long)prev_ret);
+		compression[j].l = k;
+		compression[j].c = *(prev_ret + k + 1);
+//		printf("%p\n", prev_ret);
+//		printf("%p\n", dictionary);
+		printf("(%d, %d, %d) ", compression[j].d,
+								compression[j].l,
+								compression[j].c);
+		dictionary += k + 1;
+		i += k + 1;
+		j++;
+	}
+	printf("\n");
+	printf("j: %ld\n", j);
+	printf("sizeof(t_data): %ld\n", sizeof(t_data));
+	printf("previous size: %ld\n", size);
+	printf("new size: %ld\n", j * sizeof(t_data));
 }
 
 int		get_executable(int fd, char *filename, struct stat sb)
