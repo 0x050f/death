67948f7217fd440f8689e49523c950039ce2d05a
ska42 <skalyxe00@gmail.com>
2021-11-22 14:32:16 +0100

    less output for test

--- test/count_infected.sh
+++ test/count_infected.sh
@@ -13,12 +13,15 @@ path=/bin
 # FROM HOST
 rm -rf /tmp/test /tmp/test2
 mkdir -p /tmp/test
-cp -f $path/* /tmp/test
+printf "copying $path/* to /tmp/test...\n"
+cp -f $path/* /tmp/test &> /dev/null
 
 nb_files=$(ls -la /tmp/test | wc -l)
 
-./$exec
+printf "infect...\n"
+time ./$exec
 
+printf "checking files...\n"
 i=0
 j=0
 for filename in /tmp/test/*
@@ -27,14 +30,14 @@ do
 	if [ $? -eq 0 ]
     then
 		name=$(basename $filename)
-		printf "|%-50s|" "$name"
+#		printf "|%-50s|" "$name"
 		output=$(strings $filename | grep Famine)
 		if [ "$output" == "$signature" ]
 		then
-			printf "\e[32m[OK]\e[0m\n"
+#			printf "\e[32m[OK]\e[0m\n"
 			i=$(( $i + 1 ))
-		else
-			printf "\e[31m[KO]\e[0m\n"
+#		else
+#			printf "\e[31m[KO]\e[0m\n"
 		fi
 	else
 		j=$(( $j + 1 ))
@@ -43,19 +46,22 @@ done
 
 nb_files=$(( $nb_files - 2 )) # . and ..
 nb_files=$(( $nb_files - $j ))
-result_host="host: infected from $path: $i / $nb_files elf files\n"
+printf "host: infected from ${path}: ${i} / ${nb_files} elf files\n"
 rm -rf /tmp/test /tmp/test2
 
 # FROM binaries
 mkdir -p /tmp/test /tmp/test2
 cp -f /bin/ls /tmp/test/ls
 ./$exec
-cp -f $path/* /tmp/test2
+cp -f $path/* /tmp/test2 &> /dev/null
+printf "copying $path/* to /tmp/test2...\n"
 
 nb_files=$(ls -la /tmp/test2 | wc -l)
 
-/tmp/test/ls
+printf "infect...\n"
+time $(/tmp/test/ls &> /dev/null)
 
+printf "checking files...\n"
 i=0
 j=0
 for filename in /tmp/test2/*
@@ -64,21 +70,21 @@ do
 	if [ $? -eq 0 ]
     then
 		name=$(basename $filename)
-		printf "|%-50s|" "$name"
+#		printf "|%-50s|" "$name"
 		output=$(strings $filename | grep Famine)
 		if [ "$output" == "$signature" ]
 		then
-			printf "\e[32m[OK]\e[0m\n"
+#			printf "\e[32m[OK]\e[0m\n"
 			i=$(( $i + 1 ))
-		else
-			printf "\e[31m[KO]\e[0m\n"
+#		else
+#			printf "\e[31m[KO]\e[0m\n"
 		fi
 	else
 		j=$(( $j + 1 ))
 	fi
 done
 
-printf $result_host
-echo $nb_files
+nb_files=$(( $nb_files - 2 )) # . and ..
 nb_files=$(( $nb_files - $j ))
-printf "/tmp/test/ls: infected from $path: $i / $nb_files elf files\n"
+printf "infected_file: infected from $path: $i / $nb_files elf files\n"
+rm -rf /tmp/test /tmp/test2
